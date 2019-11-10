7c66b369500da517faadc2ae13d86eb52a8d71f4
Emma Pang <emma.kpang@gmail.com>
2019-11-10 11:29:54 -0500

    finished PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,43 +12,60 @@ with open(budget_csv, newline='') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     next(csvreader)
 
     #create lists
-    dif_list=[]   
     prof_loss=[]
+    date_list = []
+    
     #set count to 0
     total=0
     datecount=0
+    maximum=0
+    minimum=0
     #loop through rows of csv
     for rows in csvreader:
         total += float(rows[1])
         datecount += 1
         prof_loss.append(rows[1])
-    #index prof_loss list
-    prof_loss_list= range(0,len(prof_loss)-1)
-
-    #find differences and add to list
-    for i in prof_loss_list:
-        dif_list.append(float(prof_loss[i+1])-float(prof_loss[i]))
+        date_list.append(rows[0])
+        dif_list=[]   
+        #index prof_loss list
+        prof_loss_list= range(0,len(prof_loss)-1)
+        for i in prof_loss_list:
+            dif_list.append(float(prof_loss[i+1])-float(prof_loss[i]))
+            if maximum <dif_list[i]:
+                maximum = dif_list[i]
+                greatest_mo = (rows[0])
+            if minimum > dif_list[i]:
+                minimum = dif_list[i]
+                greatest_dec_mo=(rows[0])
         
+    
     #find average of differences
     avg_dif=sum(dif_list)/datecount
-    maximum = max(dif_list)
-    minimum = min(dif_list)
-    
-    print("Financial Analysis")
-    print("----------------------------")
-    print("Total Months: " + str(datecount)) 
-    print("Total: " + str(total))
-    print("Average Change: " + str(avg_dif))
-    print("Greatest Increase in Profits: " + str(maximum))
-    print("Greatest Decrease in Profits: " +str(minimum))
+  
+    #print to terminal
+    print("Financial Analysis") 
+    print("----------------------")
+    print("Total Months: " + str(datecount))
+    print("Total: " + "${:.2f}".format(total))
+    print("Average Change: " + "${:.2f}".format(avg_dif))
+    print("Greatest Increase in Profits: " + greatest_mo + " "+"("+ "${:.0f}".format(maximum))
+    print("Greatest Decrease in Profits: "+ greatest_dec_mo + " "+"("+ "${:.0f}".format(minimum) +")"  )
 
-    #print(prof_loss)
-    #print(dif_list)
+    #print to text file
+    print("Financial Analysis" + "\n"+ 
+        "----------------------" +"\n"+ 
+        "Total Months: " + str(datecount) + "\n" +
+        "Total: " + "${:.2f}".format(total) + "\n" +
+        "Average Change: " + "${:.2f}".format(avg_dif) + "\n" +
+        "Greatest Increase in Profits: " + greatest_mo + " "+"("+ "${:.0f}".format(maximum) +")"+"\n"+
+        "Greatest Decrease in Profits: "+ greatest_dec_mo + " "+"("+ "${:.0f}".format(minimum) +")"
+        ,  file=open('PyBank.txt', 'w'))
+
 
 
     
